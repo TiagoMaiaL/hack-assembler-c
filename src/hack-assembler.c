d93a2dc826ec14d0a81ec0978d820b05f417bffb
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2024-09-24 22:33:29 -0300

    Implement functions to convert c and a instructions

--- src/hack-assembler.c
+++ src/hack-assembler.c
@@ -1,14 +1,156 @@
 #include <stdio.h>
 
+#define ON        '1'
+#define OFF       '0'
+#define WORD_SIZE 16
+
 struct cinst {
     char * dest;
     char * comp;
     char * jmp;
 };
 
-char * instbin(struct cinst);
+// TODO: Use the functions in stdlib
+short streq(char *, char *);
+short strin(char, char *);
+void strcp(char *from, char *to);
+
+// TODO: Document each one of these functions.
+void instbin(struct cinst, char *bin);
+void compbin(char * instcomp, char * bin);
 void destbin(char[], char[]);
 void jmpbin(char[], char[]);
+
+struct ainst {
+    char * val;
+};
+
+// TODO: Implement atoi and then use the function in stdlib
+int atoi(char *);
+void itobin(int, char *, int);
+void ainstbin(struct ainst, char *);
+int ipow(int, int);
+
+// TODO: Check if converting negative integers will be needed.
+int atoi(char *str)
+{
+    int c;
+    int i;
+    int count;
+
+    i = 0;
+    count = 0;
+    
+    while ((c = str[i]) != '\0') {
+        // Validate str (must contain only digits)
+        if (!(c >= '0' && c <= '9')) {
+            return -1; // Let's use -1 for now, until we find a better value to return here.
+        }
+        ++i;
+    }
+
+    count = i;
+    
+    short digits[i];
+
+    i = 0;
+
+    while ((c = str[i]) != '\0') {
+        short digit;
+
+        digit = c - '0';
+        digits[i] = digit;
+
+        ++i;
+    }
+
+    int result;
+    int end = count - 1;
+
+    for (i = end; i >= 0; --i) {
+        short digit = digits[i];
+        result += digit * ipow(10, end - i);
+    }
+
+    return result;
+}
+
+int ipow(int n, int exp)
+{
+    if (exp == 0)
+        return 1;
+    else if (exp == 1)
+        return n;
+    else {
+        long long result = 1;
+
+        for (int i = 0; i < exp; ++i)
+            result *= n;
+        
+        return result;
+    }
+}
+
+void itobin(int val, char *bin, int bincount) {
+    int i, res, rem;
+
+    for (i = 0; i < bincount; ++i)
+        bin[i] = OFF;
+
+    bin[bincount] = '\0';
+
+    i = 0;
+    res = val;
+    rem = 0;
+
+    while (res > 0 && i < bincount) {
+        rem = res % 2;
+        res = res / 2;
+
+        if (rem == 0) {
+            bin[i] = OFF;
+        } else {
+            bin[i] = ON;
+        }
+
+        if (res == 1) {
+            ++i;
+            bin[i] = ON;
+            res = 0;
+        }
+
+        ++i;
+    }
+
+    // Reverse the bin string.
+    // TODO: Check if there's a stdlib function for this.
+    // TODO: If there's, replace this code with it.
+    // TODO: If not, extract this into a helper function.
+    if (i > 0) {
+        int left = 0;
+        int right = bincount - 1;
+
+        while (left < right) {
+            int leftval, temp;
+
+            leftval = bin[left];
+            temp = bin[right];
+
+            bin[left] = temp;
+            bin[right] = leftval;
+
+            ++left;
+            --right;
+        }
+    }
+}
+
+void ainstbin(struct ainst inst, char *bin)
+{
+    char *valstr = inst.val;
+    int val = atoi(valstr);
+    itobin(val, bin, WORD_SIZE);
+}
 
 int main()
 {
@@ -16,27 +158,123 @@ int main()
     // 1. Parse input text into tokens
     // 2. Parse tokens into instructions or errors
     // 3. Translate instructions into bin code
+    struct ainst inst;
+    inst.val = "123";
 
-    struct cinst comp;
-    comp.dest = "D";
-    comp.comp = "";
-    comp.jmp = "JLE";
+    char bin[WORD_SIZE];
 
-    char jmpbinout[4];
-    jmpbin(comp.jmp, jmpbinout);
-
-    printf("%s", jmpbinout);
+    ainstbin(inst, bin);
+    
+    printf("@123 in binary = %s\n", bin);
 
     return 0;
 }
 
-#define ON    '1'
-#define OFF   '0'
-
-char * instbin(struct cinst comp)
+void instbin(struct cinst comp, char *bin)
 {
-    
-    return "1110011111010000";
+    strcp("111", bin);
+
+    char compbinary[8];
+    compbin(comp.comp, compbinary);
+    strcp(compbinary, bin + 3);
+
+    char destbinary[4];
+    destbin(comp.dest, destbinary);
+    strcp(destbinary, bin + 3 + 7);
+
+    char jmpbinary[4];
+    jmpbin(comp.jmp, jmpbinary);
+    strcp(jmpbinary, bin + 3 + 7 + 3);
+}
+
+void strcp(char *from, char *to)
+{
+    int i;
+
+    for (i = 0; from[i] != '\0'; ++i) {
+        to[i] = from[i];
+    }
+
+    to[i] = '\0';
+}
+
+short strin(char c, char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == c) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void compbin(char * instcomp, char * bin)
+{
+    if (strin('M', instcomp)) {
+        bin[0] = ON;
+    } else {
+        bin[0] = OFF;
+    }
+
+    bin += 1;
+
+    if (streq(instcomp, "0")) {
+        strcp("101010", bin);
+
+    } else if (streq(instcomp, "1")) {
+        strcp("111111", bin);
+
+    } else if (streq(instcomp, "-1")) {
+        strcp("111010", bin);
+   
+    } else if (streq(instcomp, "D")) {
+        strcp("001100", bin);
+ 
+    } else if (streq(instcomp, "A") || streq(instcomp, "M")) {
+        strcp("110000", bin);
+
+    } else if (streq(instcomp, "!D")) {
+        strcp("001101", bin);
+
+    } else if (streq(instcomp, "!A") || streq(instcomp, "!M")) {
+        strcp("110001", bin);
+
+    } else if (streq(instcomp, "-D")) {
+        strcp("001111", bin);
+
+    } else if (streq(instcomp, "-A") || streq(instcomp, "-M")) {
+        strcp("110011", bin);
+
+    } else if (streq(instcomp, "D+1")) {
+        strcp("011111", bin);
+
+    } else if (streq(instcomp, "A+1") || streq(instcomp, "M+1")) {
+        strcp("110111", bin);
+
+    } else if (streq(instcomp, "D-1")) {
+        strcp("001110", bin);
+
+    } else if (streq(instcomp, "A-1") || streq(instcomp, "M-1")) {
+        strcp("110010", bin);
+
+    } else if (streq(instcomp, "D+A") || streq(instcomp, "D+M")) {
+        strcp("000010", bin);
+
+    } else if (streq(instcomp, "D-A") || streq(instcomp, "D-M")) {
+        strcp("010011", bin);
+
+    } else if (streq(instcomp, "A-D") || streq(instcomp, "M-D")) {
+        strcp("000111", bin);
+
+    } else if (streq(instcomp, "D&A") || streq(instcomp, "D&M")) {
+        strcp("000000", bin);
+
+    } else if (streq(instcomp, "D|A") || streq(instcomp, "D|M")) {
+        strcp("010101", bin);
+    }
 }
 
 #define DEST_MAX_LEN 3
@@ -64,7 +302,7 @@ void destbin(char instdest[], char bin[])
         ++i;
     }
 
-    instdest[i] = '\0';
+    bin[DEST_MAX_LEN] = '\0';
 }
 
 // TODO: Move helper functions to their own file.
