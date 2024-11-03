93d691cd1d8c206ff2ae30460276f5653b58c874
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2024-11-03 01:19:49 -0300

    Add build.sh file

---
+++ build/build.sh
@@ -0,0 +1,26 @@
+cd "`dirname $0`"
+
+source_files=(
+    '../src/utils.c'
+    '../src/ainst-bin.c'
+    '../src/cinst-bin.c'
+    '../src/lexer.c'
+    '../src/parser.c'
+    '../src/symbols.c'
+    '../src/file.c'
+    '../src/main.c'
+)
+files=("${source_files[@]}")
+
+args=""
+
+if [ -f ./hasm ]; then
+    rm hasm
+    rm -rf *.dSYM
+fi
+
+for file in "${files[@]}"; do
+    args+=$file' '
+done
+
+clang -g -Wall -o hasm $args
